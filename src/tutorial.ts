b99d509524baed2a5a85e71ee0452a0673b973da
sree-r-one <sreeraj_ec@hotmail.com>
2024-05-12 08:22:56 +0800

    Code 2/100

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,12 +1,29 @@
-const books = ["1984", "Era of Darkness", "Brave New World"];
+let temperatures: number[] = [20, 30, 50];
 
-let foundBook: string | undefined;
+let colors: string[] = ["red", "green", "blue"];
 
-for (let book of books) {
-  if (book === "1984") {
-    foundBook = book;
-    break;
-  }
+let mixedArray: (number | string)[] = [1, 2, 3];
+
+let book = { title: "book", cost: 20 };
+let pen = { title: "pen", cost: 20 };
+let notebook = { title: "notebook" };
+
+let items: { readonly title: string; cost?: number }[] = [book, pen, notebook];
+
+const calculateDiscount = (price: number): number => {
+  return price * 0.9;
+};
+
+const names: string[] = ["john", "sree", "raj"];
+
+const isNameInList = (name: string): boolean => {
+  return name.includes(name);
+};
+
+let nameToCheck = "jane";
+
+if (isNameInList(nameToCheck)) {
+  console.log(`${nameToCheck} found!`);
+} else {
+  console.log("Not found!");
 }
-
-console.log(foundBook?.length);
